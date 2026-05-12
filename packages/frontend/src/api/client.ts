0548989f667e972ee0e5e0807ba675e4e90d2927
johanvaatstra-cloud <johan.vaatstra@outlook.com>
2026-05-12 10:55:41 +0200

    Update client.ts

--- packages/frontend/src/api/client.ts
+++ packages/frontend/src/api/client.ts
@@ -2,4 +2,5 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL + '/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
+  export default apiClient
 })
